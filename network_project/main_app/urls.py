896821dbf81b71f97e324832cc450e75252927d9
TJun-Jie <tjunjie2000@gmail.com>
2021-01-10 17:55:42 +0800

    edited templates

--- network_project/main_app/urls.py
+++ network_project/main_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name= 'main_app'
 
 urlpatterns = [
+    path('/forms', views.get_form, name='form'),
     path('', views.index, name='index')
 ]
--- network_project/main_app/views.py
+++ network_project/main_app/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .forms import InputForm
 from .generateGraph import create_graph
 # Create your views here.
 
@@ -6,3 +8,22 @@ image_bytes = create_graph()
 def index(request):
     context_dict = { 'data' : image_bytes}
     return render(request, 'main_app/index.html', context_dict )
+
+
+def get_form(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = InputForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponse('/thanks/')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = InputForm()
+
+    return render(request, 'main_app/forms.html', {'form': form})

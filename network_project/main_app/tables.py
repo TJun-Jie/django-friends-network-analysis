3c49370e4bd50b9d9cc38569a04a64225cc21b87
Evande <crossjab@gmail.com>
2021-01-11 23:40:54 +0800

    'tableheading'

--- network_project/main_app/tables.py
+++ network_project/main_app/tables.py
@@ -5,9 +5,18 @@ from django.db import connection
 def db_table_exists(table_name):
     return table_name in connection.introspection.table_names()
 
+def num_friends(student_object):
+    friends = Friendship1.objects.all()
+    count = 0
+    for entry in friends:
+        if student_object == entry.friend:
+            count +=1
+    return count
+
 def create_table():
     if(db_table_exists('friendship_table')):
         table_context ={}
+        # Create tables for student,friend 1-3
         for entry in Friendship1.objects.all():
             if entry.student.id not in table_context.keys():
                 name_list=[]
@@ -16,6 +25,9 @@ def create_table():
                 table_context[entry.student.id]=name_list
             else:
                 table_context[entry.student.id].append(entry.friend.name)
+        # Create tables for number of friends of student
+        for entry in Friendship1.objects.all():       
+            table_context[entry.student.id].append(num_friends(entry.student))
 
         return table_context
     else:

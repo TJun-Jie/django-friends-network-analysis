a977f989664eefb824a12a127b979ce71e18e6b2
Tai Jun Jie <tjunjie2000@gmail.com>
2021-01-11 08:51:33 +0800

    completed table

--- network_project/main_app/urls.py
+++ network_project/main_app/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name= 'main_app'
 
 urlpatterns = [
-    path('/forms', views.get_form, name='forms'),
+    path('forms/', views.get_form, name='forms'),
     path('', views.index, name='index'),
     path('tables/', views.tables, name='tables')
 ]
